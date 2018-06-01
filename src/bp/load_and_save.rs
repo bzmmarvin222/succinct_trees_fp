4ed24191626eaf1327b0a1bfa254eaa6fcb2188f
Elena Shashko <shashk00@fiws196.cs.tu-dortmund.de>
2018-06-01 09:45:11 +0200

    test_rank0 geschrieben

---
+++ src/bp/load_and_save.rs
@@ -0,0 +1,19 @@
+fn save-bit-vect()
+{}
+
+fn a0-1-string-to-bp()
+{}
+
+fn parenthesis-string-to-bp()
+{}
+
+fn bp-to-0-1-string()
+{}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
