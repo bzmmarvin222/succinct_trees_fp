b85695d5f9385ba998a2dca189afb0c1c59da1fb
Elena Shashko <shashk00@fiws196.cs.tu-dortmund.de>
2018-06-01 09:45:11 +0200

    test_rank0 geschrieben

---
+++ src/bp/bp_methods.rs
@@ -0,0 +1,48 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+
+
+
+fn test_rank0(&self, x:u32){
+let mut result;
+let mut iterator = self.vec::BitSliceBlockIter;
+let mut current = iterator.next(); //here current ist vec[1];
+while(current != None){
+    if(current == 0){
+        result = result+1;
+    }
+    current = iterator.next();
+}
+assert_eq(result, self.rank0);
+}
+
+fn test_rank1(){
+
+}
+
+fn test_is_leaf(){
+
+}
+
+fn test_findclose(){
+
+}
+
+fn test_ansector(){
+
+}
+
+fn test_parent(){
+
+}
+
+fn test_first_child(){
+
+}
+
+fn test_subtree_size(){
+
+}
+
+}
