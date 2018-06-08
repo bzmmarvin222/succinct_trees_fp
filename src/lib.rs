c0da2a8d88ffde7028de6f66515f122feb45d0d0
Elena Shashko <shashk00@fiws196.cs.tu-dortmund.de>
2018-06-08 11:43:18 +0200

    prev_0 korrect implementiert

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate serde;
 extern crate serde_derive;
 
 mod bp;
+mod louds;
 
 #[cfg(test)]
 mod tests {
--- src/louds/mod.rs
+++ src/louds/mod.rs
@@ -1,5 +1,5 @@
-//extern crate serde;
-//extern crate bincode;
+extern crate serde;
+extern crate bincode;
 extern crate bv;
 extern crate bio;
 
@@ -7,18 +7,22 @@ use self::bio::data_structures::rank_select::RankSelect;
 use self::bv::BitVec;
 use self::bincode::{serialize, deserialize};
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize)]
 pub struct LOUDS{
-    vec: BitVec
+    vec: BitVec<u8>,
+    rank_select_structure: RankSelect
 }
 
 impl LOUDS{
-    fn new(vector: BitVec) -> LOUDS {
+    fn new(vector: BitVec<u8>) -> LOUDS {
     //    k = (log(vector.bit_len()) hoch 2)/32;
-        k=1;
+        let k=1;
+        // vector is a bv BitVec<Block = usize>
+        // usize is u8 of a 64 bit mashine and u4 of a 32 bit mashine
+        // u8 vertor shall be a bv BitVec<Block = u8>
         LOUDS {
             vec: vector,
-            rank_select_structure: RankSelect::new(vec,k)
+            rank_select_structure: RankSelect::new(vector,k)
         }
     }
 
@@ -49,12 +53,22 @@ impl LOUDS{
 // from here x,y are the elements of the sequence, which
 // represent a node. So, in vec there is 0 at the Position x - 1,y - 1
 
-    fn prev_0(x : u64){
-        this.rank_select_structure.select_0(this.rank_select_structure.rank_0(x))
+    fn prev_0(&self, x : u64) -> u64 {
+        match self.rank_select_structure.rank_0(x) {
+            Some(i) => {
+                match self.rank_select_structure.select_0(i) {
+                    Some(result) => {result},
+                    None =>{0}
+                }
+            },
+            None => {0}
+            //is there are no previous 0, then will 0 returned
+            //at index 0 cannot stand 0, this value is a mark for non-exstence
+        }
     }
 
     fn next0(x : u64){
-        this.rank_select_structure.select_0(this.rank_select_structure.rank_0(x)+1)
+        //this.rank_select_structure.select_0(this.rank_select_structure.rank_0(x)+1)
     }
 
     // whether x is a leaf
