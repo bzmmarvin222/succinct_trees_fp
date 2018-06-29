73f4c45cdbf8ba2e911963bb0c0f3daf8279efbf
Alexander Greb <grebal00@fiws196.cs.tu-dortmund.de>
2018-06-29 11:47:49 +0200

    rank_1, rank_0 select_1 working and tested

--- src/bp/range_min_max_tree/mod.rs
+++ src/bp/range_min_max_tree/mod.rs
@@ -14,8 +14,8 @@ impl rmm_Node {
     pub fn new() -> rmm_Node{
         rmm_Node {
             excess : 0,
-            min_excess : 0,
-            max_excess : 0,
+            min_excess : i64::max_value(),
+            max_excess : i64::min_value(),
             count_bits : 0,
         }
     }
@@ -45,36 +45,32 @@ impl RangeMinMaxTree {
 
         let quantity_children = match bitvector.len()%blocksize {
              0 => bitvector.len()/blocksize,
-             _ => bitvector.len()/blocksize+1,
+             _ => (bitvector.len()/blocksize)+1,
          };
-         println!("vectorn length: {:?}", bitvector.len());
-         println!("quantity_children: {:?}", quantity_children);
 
          let nodeCount =  match quantity_children.checked_next_power_of_two() {
              Some(n) => 2*n -1,
              None => panic!("Overflow Error"),
          };
 
-         println!("node count{:?}", nodeCount as usize);
 
          let mut heap: Vec<rmm_Node> = Vec::with_capacity(nodeCount as usize);
 
-println!("heapcapacity {:?}", heap.capacity());
 
          for i in 0 .. heap.capacity(){
              heap.push(rmm_Node::new());
          }
 
 
-         println!("heaplength {:?}", heap.len());
-
          let mut e: i64 = 0;
-         let mut m: i64 = 0;
-         let mut M: i64 = 0;
+         let mut m: i64 = i64::max_value();
+         let mut M: i64 = i64::min_value();
+         let mut bitcount = 0;
          let mut count: u64 = 0;
          let mut currentLeaf = heap.len()/2;
 
          for i in 0 .. bitvector.len() {
+             bitcount = bitcount +1;
              if bitvector[i] {
                  e += 1;
              }
@@ -83,56 +79,99 @@ println!("heapcapacity {:?}", heap.capacity());
              }
              m = cmp::min(e,m);
              M = cmp::max(e,M);
-             if (i+1) % blocksize == 0 {
+             if (i+1) == bitvector.len(){
                  heap[currentLeaf].excess = e;
                  heap[currentLeaf].min_excess = m;
                  heap[currentLeaf].max_excess = M;
-                 heap[currentLeaf].count_bits = blocksize;
+                 heap[currentLeaf].count_bits = bitcount;
+             }
+             else if ((i+1) % blocksize == 0) {
+                 heap[currentLeaf].excess = e;
+                 heap[currentLeaf].min_excess = m;
+                 heap[currentLeaf].max_excess = M;
+                 heap[currentLeaf].count_bits = bitcount;
                  currentLeaf +=1;
                  e=0;
-                 m=0;
-                 M=0;
+                 if bitvector[i+1] {
+                     m=1;
+                     M=1;
+                 }
+                 else {
+                     m=-1;
+                     M=-1;
+                 }
+                 bitcount = 0;
              }
          }
 
-         for x in (heap.len()/2)-1 .. 0 {
-             println!("{:?}", x);
-             let left = 2*x+1;
-             let right = 2*x+2;
+
+
+         let mut x = heap.len()/2;
+
+         while x > 0 {
+
+             let mut node = x -1;
+             let left = 2*node+1;
+             let right = 2*node+2;
+
+
+
              e = heap[left].excess + heap[right].excess;
-             m = cmp::min(heap[left].min_excess, heap[left].excess + heap[right].min_excess);
-             M = cmp::max(heap[left].max_excess, heap[left].excess + heap[right].max_excess);
+             if (heap[left].min_excess == i64::max_value()) {
+                 m = i64::max_value();
+                 M = i64::min_value();
+             }
+             else {
+                 let right_min = i64::checked_add(heap[left].excess, heap[right].min_excess).unwrap_or(i64::max_value());
+                 m = cmp::min(heap[left].min_excess, right_min);
+                 let right_max = i64::checked_add(heap[left].excess, heap[right].max_excess).unwrap_or(i64::min_value());
+                 M = cmp::max(heap[left].max_excess, right_max);
+             }
              count = heap[left].count_bits + heap[right].count_bits;
-             heap[x].excess = e;
-             heap[x].min_excess = m;
-             heap[x].max_excess = M;
-             heap[x].count_bits = count;
+             if left >= heap.len() || right >= heap.len() {
+                 heap[node].excess = 0;
+                 heap[node].min_excess = i64::max_value();
+                 heap[node].max_excess = i64::min_value();
+                 heap[node].count_bits = 0;
+             } else {
+                 heap[node].excess = e;
+                 heap[node].min_excess = m;
+                 heap[node].max_excess = M;
+                 heap[node].count_bits = count;
+             }
+             x =x-1;
          }
 
          heap
     }
 
     fn rank_1(&self, i: usize) -> Option<usize>{
-        if i >= self.bitvector.len() as usize {
+        if (i >= self.bitvector.len() as usize) || (i == 0) {
             None
         }
         else {
-            let mut currentLeaf = (self.tree.len()/2) + (i/self.blocksize);
-            let mut startbit = (currentLeaf - self.tree.len()/2) * self.blocksize;
+            let bit = i-1;
+            let mut currentLeaf = (self.tree.len()/2) + (bit/self.blocksize);
+            let mut j = (currentLeaf - self.tree.len()/2) * self.blocksize;
             let mut rank: u64 = 0;
-            for j in startbit .. i {
+            while j <= bit {
                 if self.bitvector[j as u64] {
                     rank += 1;
                 }
+                j +=1;
             }
-            let  mut parent = (currentLeaf - 1) /2;
-            while parent > 0 {
-                let left = 2*parent +1;
-                let mut temp = self.tree[left].count_bits as i64 + self.tree[left].excess;
-                assert!(temp >= 0);
-                rank += temp as u64 / 2;
-                let parent = (currentLeaf - 1) /2;
+            println!("rank: {:?}", rank);
+            while currentLeaf > 0 {
+                if currentLeaf % 2 == 0 {
+                    let parent = (currentLeaf -1) /2;
+                    let left = 2*parent +1;
+                    let mut temp = self.tree[left].count_bits as i64 + self.tree[left].excess;
+                    assert!(temp >= 0);
+                    rank += (temp as u64) / 2;
+                }
+                currentLeaf = (currentLeaf - 1) /2;
             }
+            println!("after");
             Some(rank as usize)
         }
 
@@ -159,11 +198,21 @@ println!("heapcapacity {:?}", heap.capacity());
         if node >= (self.tree.len()/2) {
             let startbit = (node - self.tree.len()/2) * self.blocksize;
             let mut select = 0;
-            for j in startbit .. startbit + (self.blocksize-1) {
+            let mut tmp = 0;
+            let mut j = startbit;
+            while j <= (startbit + (self.blocksize-1)) {
+                select +=1;
+                println!("j {:?}", j);
+                println!("select {:?}", select);
                 if self.bitvector[j as u64] {
-                    select += 1;
+                    tmp += 1;
+                    if tmp == k {
+                        break;
+                    }
                 }
+                j +=1;
             }
+            println!("select {:?}", select);
             select
         }
         else if (self.tree[2*node+1].count_bits as i64 + self.tree[2*node+1].excess)/2 >= k as i64 {
@@ -173,6 +222,9 @@ println!("heapcapacity {:?}", heap.capacity());
             let temp = k as i64 - (self.tree[2 * node + 1 as usize].count_bits as i64 +
           self.tree[2 * node + 1 as usize].excess as i64) / 2 as i64;
             assert!( temp >= 0);
+            println!("bits left{:?}", self.tree[2*node+1 as usize].count_bits as usize);
+            println!("to add{:?}", self.select_1_from_tree(temp as usize,
+            2 * node + 2 as usize));
             self.tree[2*node+1 as usize].count_bits as usize + self.select_1_from_tree(temp as usize,
             2 * node + 2 as usize)
         }
@@ -191,11 +243,21 @@ println!("heapcapacity {:?}", heap.capacity());
         if node >= (self.tree.len()/2) {
             let startbit = (node - self.tree.len()/2) * self.blocksize;
             let mut select = 0;
-            for j in startbit .. startbit + (self.blocksize-1) {
+            let mut tmp = 0;
+            let mut j = startbit;
+            while j <= (startbit + (self.blocksize-1)) {
+                select +=1;
+                println!("j {:?}", j);
+                println!("select {:?}", select);
                 if !self.bitvector[j as u64] {
-                    select += 1;
+                    tmp += 1;
+                    if tmp == k {
+                        break;
+                    }
                 }
+                j +=1;
             }
+            println!("select {:?}", select);
             select
         }
         else if (self.tree[2*node+1].count_bits as i64 - (self.tree[2*node+1].count_bits as i64 + self.tree[2*node+1].excess)/2) >= k as i64{
--- src/bp/range_min_max_tree/tests.rs
+++ src/bp/range_min_max_tree/tests.rs
@@ -3,57 +3,229 @@ use super::bv::*;
 use bp::range_min_max_tree::*;
 use std::cmp;
 
+
 #[test]
-fn test_build_from_bv() {
-    let bitvec = bit_vec![true, true, false, true, false, false];
+fn test_build_tree() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let node_0 = rmm_Node {
+        excess : 0,
+        min_excess : 0,
+        max_excess : 4,
+        count_bits : 22,};
+
     let node_1 = rmm_Node {
-        excess : 2,
+        excess : 4,
         min_excess : 1,
-        max_excess : 2,
-        count_bits : 2,};
+        max_excess : 4,
+        count_bits : 16,};
 
     let node_2 = rmm_Node {
-        excess : 0,
-        min_excess : -1,
+        excess : -4,
+        min_excess : -4,
         max_excess : 0,
-        count_bits : 2,};
+        count_bits : 6,};
 
     let node_3 = rmm_Node {
+        excess : 4,
+        min_excess : 1,
+        max_excess : 4,
+        count_bits : 8,};
+
+    let node_4 = rmm_Node {
+        excess : 0,
+        min_excess : -3,
+        max_excess : 0,
+        count_bits : 8,};
+
+    let node_5 = rmm_Node {
+        excess : -4,
+        min_excess : -4,
+        max_excess : 0,
+        count_bits : 6,};
+
+    let node_6 = rmm_Node {
+        excess : 0,
+        min_excess : i64::max_value(),
+        max_excess : i64::min_value(),
+        count_bits : 0,};
+
+    let node_7 = rmm_Node {
+        excess : 2,
+        min_excess : 1,
+        max_excess : 3,
+        count_bits : 4,};
+
+    let node_8 = rmm_Node {
+        excess : 2,
+        min_excess : 0,
+        max_excess : 2,
+        count_bits : 4,};
+
+    let node_9 = rmm_Node {
+        excess : -2,
+        min_excess : -3,
+        max_excess : -1,
+        count_bits : 4,};
+
+    let node_10 = rmm_Node {
+        excess : 2,
+        min_excess : -1,
+        max_excess : 2,
+        count_bits : 4,};
+
+    let node_11 = rmm_Node {
+        excess : -2,
+        min_excess : -2,
+        max_excess : 0,
+        count_bits : 4,};
+
+    let node_12 = rmm_Node {
         excess : -2,
         min_excess : -2,
         max_excess : -1,
         count_bits : 2,};
 
-    let node_4 = rmm_Node {
+    let node_13 = rmm_Node {
         excess : 0,
-        min_excess : 0,
-        max_excess : 0,
+        min_excess : i64::max_value(),
+        max_excess : i64::min_value(),
         count_bits : 0,};
 
-    let node_5 = rmm_Node {
-        excess : 2,
-        min_excess : 1,
-        max_excess : 2,
-        count_bits : 4,};
-
-    let node_6 = rmm_Node {
-        excess : -2,
-        min_excess : 1,
-        max_excess : -1,
-        count_bits : 2,};
-
-
-    let node_7 = rmm_Node {
+    let node_14 = rmm_Node {
         excess : 0,
-        min_excess : 0,
-        max_excess : 2,
-        count_bits : 6,};
-
-    let expected = vec![node_7, node_5, node_6, node_1, node_2, node_3, node_4];
+        min_excess : i64::max_value(),
+        max_excess : i64::min_value(),
+        count_bits : 0,};
 
 
-    let tree = RangeMinMaxTree::new(bitvec, 3);
+    let expected = vec![node_0, node_1, node_2, node_3, node_4, node_5, node_6, node_7, node_8, node_9, node_10, node_11, node_12, node_13, node_14];
+
+
+    let tree = RangeMinMaxTree::new(bitvec, 4);
     assert_eq!(tree.tree, expected)
+}
 
+#[test]
+fn nonvalid_i_rank_1() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.rank_1(u64::max_value() as usize), None);
+}
 
+#[test]
+fn nonvalid_i_is_zero_rank_1() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.rank_1(0), None);
+}
+
+#[test]
+fn valid_rank_1() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.rank_1(11).unwrap(), 6);
+}
+
+#[test]
+fn nonvalid_i_rank_0() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.rank_0(u64::max_value() as usize), None);
+}
+
+#[test]
+fn nonvalid_i_is_zero_rank_0() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.rank_0(0), None);
+}
+
+#[test]
+fn valid_rank_0() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.rank_0(11).unwrap(), 5);
+}
+
+#[test]
+fn valid_select_1() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.select_1(7).unwrap(),12);
+}
+
+#[test]
+fn nonvalid_i_is_zero_select_1() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.select_1(0), None);
+}
+
+#[test]
+fn nonvalid_i_select_1() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.select_1(u64::max_value() as usize), None);
+}
+
+#[test]
+fn valid_select_0() {
+    let bitvec = bit_vec![true, true, true, false,
+                        true, false, true, true,
+                        false, false, false, true,
+                        false, true, true, true,
+                        false, true, false, false,
+                        false, false];
+    let tree = RangeMinMaxTree::new(bitvec, 4);
+    assert_eq!(tree.select_0(3).unwrap(),9);
 }
