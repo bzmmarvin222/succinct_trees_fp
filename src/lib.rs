bf98d6659bbb7d29abbbaee561c88d1db3aff591
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-07-19 01:11:39 +0200

    added benches

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ extern crate serde;
 extern crate serde_derive;
 extern crate bv;
 extern crate bincode;
+extern crate rand;
 
 use bincode::serialize;
 use bincode::deserialize;
@@ -46,11 +47,194 @@ mod tests {
     use super::*;
     use louds::LOUDS;
     use bp::BalancedParentheses;
+    use rand::prelude::*;
+    use bv::BitsMut;
+
+    fn rand_big_tree_bp() -> BitVec {
+        let upper_limit = 1000000000;
+        let mut result = BitVec::new_fill(true, upper_limit);
+        let mut current_depth = 1;
+        for index in 1..upper_limit {
+            let rand: bool = random();
+            if !rand {
+                result.set_bit(index, rand);
+                current_depth -= 1;
+            } else if upper_limit - index == current_depth {
+                result.set_bit(index, false);
+                current_depth -= 1;
+            } else {
+                current_depth += 1;
+            }
+        }
+        result
+    }
+    fn rand_big_tree_louds() -> BitVec<u8> {
+        let upper_limit = 1000000000;
+        let mut result: BitVec<u8> = BitVec::new_fill(true, upper_limit);
+        let mut current_depth = 1;
+        for index in 1..upper_limit {
+            let rand: bool = random();
+            if !rand {
+                result.set_bit(index, rand);
+                current_depth -= 1;
+            } else if upper_limit - index == current_depth {
+                result.set_bit(index, false);
+                current_depth -= 1;
+            } else {
+                current_depth += 1;
+            }
+        }
+        result
+    }
+    #[bench]
+    fn bench_bp_subtree_size(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        b.iter(|| bp.subtree_size(0));
+    }
 
     #[bench]
-    fn bench_add_two(b: &mut Bencher) {
-        let braces = String::from("((())(())()()(()())");
-        let bp = BalancedParentheses::from_braces_representation(braces);
-        b.iter(|| bp.subtree_size(0));
+    fn bench_louds_subtree_size(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        b.iter(|| louds.subtree_size(0));
+    }
+
+    #[bench]
+    fn bench_bp_index_represents_node(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.index_represents_node(index));
+    }
+
+    #[bench]
+    fn bench_louds_index_represents_node(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.index_represents_node(index));
+    }
+
+    #[bench]
+    fn bench_bp_is_leaf(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.is_leaf(index));
+    }
+
+    #[bench]
+    fn bench_louds_is_leaf(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.is_leaf(index));
+    }
+
+    #[bench]
+    fn bench_bp_parent(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.parent(index));
+    }
+
+    #[bench]
+    fn bench_louds_parent(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.parent(index));
+    }
+
+    #[bench]
+    fn bench_bp_next_sibling(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.next_sibling(index));
+    }
+
+    #[bench]
+    fn bench_louds_next_sibling(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.next_sibling(index));
+    }
+
+    #[bench]
+    fn bench_bp_first_child(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.first_child(index));
+    }
+
+    #[bench]
+    fn bench_louds_first_child(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.first_child(index));
+    }
+
+    #[bench]
+    fn bench_bp_degree(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.degree(index));
+    }
+
+    #[bench]
+    fn bench_louds_degree(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.degree(index));
+    }
+
+    #[bench]
+    fn bench_bp_child(b: &mut Bencher) {
+        let bp = BalancedParentheses::new(rand_big_tree_bp());
+        let mut index = 133742;
+        while !bp.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| bp.child(index, 3));
+    }
+
+    #[bench]
+    fn bench_louds_child(b: &mut Bencher) {
+        let louds = LOUDS::new(rand_big_tree_louds());
+        let mut index = 133742;
+        while !louds.index_represents_node(index) {
+            index += 1;
+        }
+        b.iter(|| louds.child(index, 3));
     }
 }
